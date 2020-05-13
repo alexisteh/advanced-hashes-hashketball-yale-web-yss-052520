4cbb4d1ce07d192f4a87f5b1159a63d8084611cc
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-13 00:45:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -183,8 +183,8 @@ def player_stats (pname)
   game_hash.each do |hoaw,specs|
     specs[:players].each_with_index do |val, index| 
       if val[:player_name] == pname  
-        puts val 
-        return val
+        k = val.shift
+        return k 
       end 
     end 
   end 
